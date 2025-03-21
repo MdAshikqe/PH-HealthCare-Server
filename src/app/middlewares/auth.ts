2a5d2ebe2ba4264b6352f9b1cf1b8409bde37b32
MdAshique <asikhosen865575@gmail.com>
2025-03-21 12:12:41 +0600

    handaling ApiErrors

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,18 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { Secret } from "jsonwebtoken"
 import config from "../../config";
+import ApiError from "../errors/ApiErrors";
+import status from "http-status";
 
 const auth=(...roles:string[])=>{
     return async (req:Request,res:Response,next:NextFunction)=>{
        try {
             const token=req.headers.authorization;
-            console.log({token});
             if(!token){
-                throw new Error("You are not authorized")
+                throw new ApiError(status.UNAUTHORIZED, "You are not authorized","")
             }
             const verifyUser=jwt.verify(token,config.jwt.access_token_secret as Secret)
             if (roles.length && typeof verifyUser !== "string" && !roles.includes(verifyUser.role)) {
-                throw new Error("You are not authrized")
+                throw new ApiError(status.FORBIDDEN,"forbidden",'')
             }
             next()
         
